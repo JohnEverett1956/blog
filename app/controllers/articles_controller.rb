7ca7b30ed4fc0ecf5ab075ff2a124b7de361248b
John Everett <john.everett@shaw.ca>
2017-03-26 17:34:10 +0000

    adding list action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+ 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
   
@@ -9,14 +17,7 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
   
-  def show
-    @article = Article.find(params[:id])
-  end
- 
-  def new
-  end
 
- 
   private
   def article_params
     params.require(:article).permit(:title, :text)
